7eefb5ac6e0fc3481c8d555c6f3f873763f35534
Alexandre Werner <xandewerner.2@gmail.com>
2020-01-11 16:10:15 -0300

    add config to transform pricing-service to microservice

--- pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
+++ pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
@@ -1,23 +1,32 @@
 package com.udacity.pricing.domain.price;
 
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
 import java.math.BigDecimal;
+import java.util.Objects;
 
 /**
  * Represents the price of a given vehicle, including currency.
  */
+@EnableEurekaClient
 public class Price {
+
+    @Id
+    @Column(name = "ID")
+    private Long vehicleId;
 
     private String currency;
     private BigDecimal price;
-    private Long vehicleId;
 
     public Price() {
     }
 
-    public Price(String currency, BigDecimal price, Long vehicleId) {
+    public Price(Long vehicleId, String currency, BigDecimal price ) {
+        this.vehicleId = vehicleId;
         this.currency = currency;
         this.price = price;
-        this.vehicleId = vehicleId;
     }
 
     public String getCurrency() {
@@ -42,5 +51,29 @@ public class Price {
 
     public void setVehicleId(Long vehicleId) {
         this.vehicleId = vehicleId;
+    }
+
+    @Override
+    public String toString() {
+        return "Price{" +
+                "vehicleId=" + vehicleId +
+                ", currency='" + currency + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Price)) return false;
+        Price price1 = (Price) o;
+        return Objects.equals(getVehicleId(), price1.getVehicleId()) &&
+                Objects.equals(getCurrency(), price1.getCurrency()) &&
+                Objects.equals(getPrice(), price1.getPrice());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getVehicleId(), getCurrency(), getPrice());
     }
 }
