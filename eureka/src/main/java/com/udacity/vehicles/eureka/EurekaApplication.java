13baf50edfafb446e3bbb93c22e94f55d2567241
Alexandre Werner <xandewerner.2@gmail.com>
2020-01-21 20:44:49 -0300

    [eureka-server] add documentation

--- eureka/src/main/java/com/udacity/vehicles/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/udacity/vehicles/eureka/EurekaApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * Launches a Spring Boot application for the Eureka Server,
+ * the princing api will be registered here.
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaApplication {
